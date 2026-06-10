ff45c61f7c8467c201eeb7bd83ee40a9ce175ae5
ghogharifenil <ghogharifenil601@gmail.com>
2026-06-10 14:33:16 +0530

    Add feture

--- apps/context_processors.py
+++ apps/context_processors.py
@@ -1,9 +1,18 @@
 from .models import Cart
+from django.db.models import Sum
 
 def cart_count(request):
+
     if request.session.get('customer_id'):
+
         customer_id = request.session.get('customer_id')
-        count = Cart.objects.filter(customer_id=customer_id).count()
+
+        count = Cart.objects.filter(
+            customer_id=customer_id
+        ).aggregate(
+            total=Sum('quantity')
+        )['total'] or 0
+
     else:
         count = 0
 
