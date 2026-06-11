6676ed7d27eee83b03e711527fcc6f975d602ab2
ghogharifenil <ghogharifenil601@gmail.com>
2026-06-11 17:30:41 +0530

    fix issus

--- shopaura/settings.py
+++ shopaura/settings.py
@@ -106,8 +106,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
-    'API_KEY': config('API_KEY'),
-    'API_SECRET': config('API_SECRET'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
 
