4f6e40171828d28aa1c1d0dbba31cd2d06748771
ghogharifenil <ghogharifenil601@gmail.com>
2026-06-11 10:59:45 +0530

    issu fix

--- apps/apps.py
+++ apps/apps.py
@@ -1,17 +1,9 @@
 from django.apps import AppConfig
 
+
 class AppsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps'
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        if not User.objects.filter(email="ghogharifenil601@gmail.com").exists():
-            User.objects.create_superuser(
-                email="ghogharifenil601@gmail.com",
-                name="Fenil",
-                city="Surat",
-                password="0121"
-            )
+        pass
